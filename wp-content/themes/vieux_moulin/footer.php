03de05d8a1200ae6c8b1fea8da665e513c0f70c0
BriolVictoria <victoria.briol@student.hepl.be>
2025-05-14 18:15:54 +0200

    Added few more CSS and added footer + maps

--- wp-content/themes/vieux_moulin/footer.php
+++ wp-content/themes/vieux_moulin/footer.php
@@ -1,37 +1,51 @@
 </main>
 
 <footer>
+    <section class="footer">
 
-    <section class="top_header">
-        <img class="logo" src="<?= get_site_icon_url('106') ?>" alt="Icône du site, initial de Vieux Moulin">
-        <p>
-            ASBL Le Vieux Moulin Service Résidentiel Général
-        </p>
-        <h2>Nous contacter</h2>
-        <p>Tel</p>
-        <p>Email</p>
-        <p>Lien</p>
-        <p>Direction</p>
-        <h2>Navigation</h2>
-        <ul >
-            <?php foreach (dw_get_navigation_links('header-menu') as $link): ?>
-                <li class="lien">
-                    <a href="<?= $link->href ?>" class="lien"><?= $link->label;?></a>
-                </li>
-            <?php endforeach;  ?>
-        </ul>
-    </section>
+        <section>
+            <img class="logo_footer" src="<?= get_site_icon_url('106') ?>" alt="Icône du site, initial de Vieux Moulin">
+            <p class="asbl_footer">ASBL Le Vieux Moulin <br>Service Résidentiel Général</p>
+        </section>
 
-    <section class="bottom_footer">
-        <p class="blog_info">© 2025 <?= get_bloginfo('name'); ?>. Tous droit réservé. Créé par Victoria Briol</p>
-        <p class="mention">Mentions légales</p>
-    </section>
-    
-    <section>
-        <!--Image carte-->
-    </section>
+        <section>
+            <div class="contacter">
+                <h2>Nous contacter</h2>
+                <p>063/60.11.50</p>
+                <p>srglevieuxmoulin@levm.be</p>
+                <p>Rue des Vennes, n°1 Strainchamps 6637 <br>Fauvillers</p>
+                <p>SCHIMIT Jean-François GSM: 0488/774.850 <br>direction@levm.be</p>
+            </div>
+        </section>
 
+        <section>
+            <div class="navigation">
+                <h2>Navigation</h2>
+                <ul class="navigation_footer">
+                    <?php foreach (dw_get_navigation_links('footer-menu') as $link): ?>
+                        <li class="lien_footer">
+                            <a href="<?= $link->href ?>" class="lien_navigation_footer"><?= $link->label;?></a>
+                        </li>
+                    <?php endforeach;  ?>
+                </ul>
+            </div>
+        </section>
+
+        <section class="carte">
+            <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.857971779085!2d5.6788238!3d49.877847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47eaa07fd84008a7%3A0x53bf82318872921c!2sRue%20des%20Vennes%2C%206637%20Fauvillers!5e0!3m2!1sfr!2sbe!4v1715685812345!5m2!1sfr!2sbe"
+                    width="365"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+
+        </section>
+
+    </section>
 </footer>
+
 
 </body>
 </html>
--- wp-content/themes/vieux_moulin/front-page.php
+++ wp-content/themes/vieux_moulin/front-page.php
@@ -6,6 +6,7 @@ $titre_section_decouvrir = get_field('titre_section_decouvrir');
 $description_decouvrir = get_field('description_decouvrir');
 $image_decouvrir = get_field('image_decouvrir');
 $bouton_decouvrir = get_field('bouton_decouvrir');
+$one_line_image_decouvrir = get_field('one_line_image_decouvrir');
 
 $titre_actualite = get_field('titre_actualite');
 $bouton_actualite = get_field('bouton_actualite');
@@ -16,6 +17,7 @@ $bouton_foyer = get_field('bouton_foyer');
 $titre_section_benevolat = get_field('titre_section_benevolat');
 $description_benevolat = get_field('description_benevolat');
 $image_benevolat = get_field('image_benevolat');
+$one_line_image_benevolat = get_field('one_line_image_benevolat');
 
 $titre_section_partenaires = get_field('titre_section_partenaires');
 $bouton_partenaire = get_field('bouton_partenaire');
@@ -29,83 +31,80 @@ $titre_section_projets = get_field('titre_section_projets');
 $description_projets = get_field('description_projets');
 $image_projets = get_field('image_projets');
 $bouton_projets = get_field('bouton_projets');
+$one_line_image_projet = get_field('one_line_image_projet');
 
-$foyers = new WP_Query([
+/*$foyers = new WP_Query([
     'post_type' => 'foyer',
     'order' => 'DESC',
     'orderby' => 'date',
     'posts_per_page' => 2,
-]);
+]);*/
 ?>
 
 <section class="decouvrir">
+    <div class="back_image_decouvrir"></div>
     <img class="image_decouvrir" src="<?= $image_decouvrir['url'] ?>" alt="<?= $image_decouvrir['alt'] ?>">
     <div class="decouvrir_container">
         <h2 class="titre_decouvrir"><?= $titre_section_decouvrir ?></h2>
         <p class="description_decouvrir"><?= $description_decouvrir ?></p>
         <a class="bouton_decouvrir" href="<?= $bouton_decouvrir['url'] ?>"><?= $bouton_decouvrir['title'] ?></a>
-
     </div>
+    <img class="one_line_image_decouvrir" src="<?= $one_line_image_decouvrir['url'] ?>" alt="<?= $one_line_image_decouvrir['alt'] ?>">
 </section>
 
 <section class="actualite">
-    <h2 class="titre_actualite"><?= $titre_actualite ?></h2>
+    <h2 class="titre_actualite">
+        <span class="titre_soulignement_actualite"><?= $titre_actualite ?></span>
+    </h2>
     <a class="bouton_actualite" href="<?= $bouton_actualite['url'] ?>"><?= $bouton_actualite['title'] ?></a>
 </section>
 
 <section class="foyer">
-    <h2 class="titre_foyer"><?= $titre_section_foyer ?></h2>
-
-    <?php if($foyers->have_posts()): while($foyers->have_posts()): $foyers->the_post(); ?>
-        <article class="projet">
-
-            <div class="projet_card">
-                <figure class="trip_fig">
-                    <?= get_the_post_thumbnail(size: 'medium', attr: ['class' => 'trip_img']); ?>
-                </figure>
-                <header class="projet_head">
-                    <h3 class="projet_title"><strong class="soulignement_carte_par_projet"><?= get_the_title(); ?></strong></h3>
-                </header>
-                <p class="description_projet"><?= get_the_excerpt() ?></p>
-            </div>
-        </article>
-    <?php endwhile; else: ?>
-        <p>Je n'ai pas de voyages récents à montrer pour le moment...</p>
-    <?php endif; ?>
-
+    <h2 class="titre_foyer">
+        <span class="titre_soulignement_foyer"><?= $titre_section_foyer ?></span>
+    </h2>
     <a class="bouton_foyer" href="<?= $bouton_foyer['url'] ?>"><?= $bouton_foyer['title'] ?></a>
 </section>
 
 <section class="benevolat">
+    <img class="one_line_image_benevolat" src="<?= $one_line_image_benevolat['url'] ?>" alt="<?= $one_line_image_benevolat['alt'] ?>">
     <div class="benevolat_container">
         <h2 class="titre_benevolat"><?= $titre_section_benevolat ?></h2>
         <p class="description_benevolat"><?= $description_benevolat ?></p>
     </div>
+    <div class="back_image_benevolat"></div>
     <img class="image_benevolat" src="<?= $image_benevolat['url'] ?>" alt="<?= $image_benevolat['alt'] ?>">
 </section>
 
 <section class="partenaires">
-    <h2 class="titre_partenaires"><?= $titre_section_partenaires ?></h2>
+    <h2 class="titre_partenaires">
+        <span class="titre_soulignement_partenaires"><?= $titre_section_partenaires ?></span>
+    </h2>
     <a class="bouton_partenaires" href="<?= $bouton_partenaire['url'] ?>"><?= $bouton_partenaire['title'] ?></a>
 </section>
 
 <section class="famille">
-    <h2 class="titre_famille"><?= $titre_section_famille ?></h2>
-    <img class="image_famille" src="<?= $image_famille['url'] ?>" alt="<?= $image_famille['alt'] ?>">
+    <h2 class="titre_famille">
+        <span class="titre_soulignement_famille"><?= $titre_section_famille ?></span>
+    </h2>
     <div class="famille_container">
-        <p class="description_famille"><?= $description_famille ?></p>
-        <a class="bouton_famille" href="<?= $bouton_famille['url'] ?>"><?= $bouton_famille['title'] ?></a>
+        <img class="image_famille" src="<?= $image_famille['url'] ?>" alt="<?= $image_famille['alt'] ?>">
+        <div class="contenu_famille">
+            <p class="description_famille"><?= $description_famille ?></p>
+            <a class="bouton_famille" href="<?= $bouton_famille['url'] ?>"><?= $bouton_famille['title'] ?></a>
+        </div>
     </div>
 </section>
 
+
 <section class="projet">
+    <img class="one_line_image_projet" src="<?= $one_line_image_projet['url'] ?>" alt="<?= $one_line_image_projet['alt'] ?>">
     <div class="projet_container">
         <h2 class="titre_projet"><?= $titre_section_projets ?></h2>
         <p class="description_projet"><?= $description_projets ?></p>
         <a class="bouton_projet" href="<?= $bouton_projets['url'] ?>"><?= $bouton_projets['title'] ?></a>
     </div>
     <img class="image_projet" src="<?= $image_projets['url'] ?>" alt="<?= $image_projets['alt'] ?>">
-
 </section>
 
 <?php get_footer(); ?>
