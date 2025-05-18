294fdc10ba8165b9e2769b3e8541ec81de95fff4
BriolVictoria <victoria.briol@student.hepl.be>
2025-05-18 18:13:44 +0200

    Added few css + the contact page css

--- wp-content/themes/vieux_moulin/page-template/template-formulaire.php
+++ wp-content/themes/vieux_moulin/page-template/template-formulaire.php
@@ -4,12 +4,48 @@ get_header();
 
 $titre_formulaire = get_field('titre_formulaire');
 $bouton_formulaire = get_field('bouton_formulaire');
+$one_line_image_formulaire = get_field('one_line_image_formulaire');
 
 ?>
 
-<section>
-    <h2><?= $titre_formulaire ?></h2>
-    <a href="<?= $bouton_formulaire['url'] ?>"><?= $bouton_formulaire['title'] ?></a>
+<section class="form">
+    <h2 class="form_title"><?= $titre_formulaire ?></h2>
+    <form class="form_container" action="#" method="get">
+        <div class="form_row double">
+            <div class="nom">
+                <label class="nom_form" for="name">
+                    <span class="titre_soulignement_form">Nom</span>
+                </label>
+                <input class="champs_form" type="text" name="name" id="name">
+            </div>
+
+            <div class="prenom">
+                <label class="nom_form" for="prenom">
+                    <span class="titre_soulignement_form">Prenom</span>
+                </label>
+                <input class="champs_form" type="text" name="prenom" id="prenom">
+            </div>
+        </div>
+
+
+        <div class="form_row email">
+            <label class="nom_form" for="email">
+                <span class="titre_soulignement_form">Email</span>
+            </label>
+            <input class="champs_form" type="email" name="email" id="email">
+        </div>
+
+        <div class="form_row note">
+            <label class="nom_form" for="note">
+                <span class="titre_soulignement_form">Note</span>
+            </label>
+            <textarea class="champs_form" name="note" id="note" cols="30" rows="5"></textarea>
+        </div>
+
+
+    </form>
+    <a class="form_boutton" href="<?= $bouton_formulaire['url'] ?>"><?= $bouton_formulaire['title'] ?></a>
+    <img class="one_line_image_formulaire" src="<?= $one_line_image_formulaire['url'] ?>" alt="<?= $one_line_image_formulaire['alt'] ?>">
 </section>
 
 <?php get_footer(); ?>
--- wp-content/themes/vieux_moulin/page-template/template-journee.php
+++ wp-content/themes/vieux_moulin/page-template/template-journee.php
@@ -5,27 +5,100 @@ get_header();
 $titre_introduction = get_field('titre_introduction');
 $description_introduction = get_field('description_introduction');
 $image_introduction = get_field('image_introduction');
+$one_line_image_intro = get_field('one_line_image_intro');
 
 $titre_faq = get_field('titre_faq');
 
 $titre_section_foyer = get_field('titre_section_foyer');
 $bouton_foyer = get_field('bouton_foyer');
+$one_line_image_foyer = get_field('one_line_image_foyer');
+
+$titre_galerie = get_field('titre_galerie');
+$galerie_photo = get_field('galerie_photo');
+$one_line_image_galerie = get_field('one_line_image_galerie');
+
+$foyers = new WP_Query([
+    'post_type' => 'foyer',
+    'order' => 'DESC',
+    'orderby' => 'date',
+    'posts_per_page' => 2,
+]);
 
 ?>
 
-<section>
-    <h2><?= $titre_introduction ?></h2>
-    <p><?= $description_introduction ?></p>
-    <img src="<?= $image_introduction['url'] ?>" alt="<?= $image_introduction['title'] ?>">
+<section class="journee_intro">
+    <img class="one_line_image_intro" src="<?= $one_line_image_intro['url'] ?>"
+         alt="<?= $one_line_image_intro['title'] ?>">
+    <div class="intro_container_journee">
+        <h2 class="journee_intro_titre"><?= $titre_introduction ?></h2>
+        <p class="journee_intro_description"><?= $description_introduction ?></p>
+    </div>
+    <img class="journee_intro_image" src="<?= $image_introduction['url'] ?>" alt="<?= $image_introduction['title'] ?>">
 </section>
 
-<section>
-    <h2><?= $titre_faq ?></h2>
+<section class="foyer">
+    <img class="one_line_image_foyer" src="<?= $one_line_image_foyer['url'] ?>"
+         alt="<?= $one_line_image_foyer['alt'] ?>">
+    <h2 class="titre_foyer">
+        <span class="titre_soulignement_foyer"><?= $titre_section_foyer ?></span>
+    </h2>
+
+    <div class="foyer_container">
+
+        <?php if ($foyers->have_posts()): while ($foyers->have_posts()): $foyers->the_post(); ?>
+            <article class="foyer_post">
+                <figure class="foyer_fig">
+                    <?= get_the_post_thumbnail(size: 'medium', attr: ['class' => 'foyer_img']); ?>
+                </figure>
+
+                <!-- Bloc texte -->
+                <div class="foyer_content">
+                    <header class="foyer_head">
+                        <h3 class="foyer_title"><?= get_the_title(); ?></h3>
+                    </header>
+                    <p class="description_foyer"><?= get_the_excerpt() ?></p>
+                </div>
+            </article>
+        <?php endwhile; else: ?>
+            <p>Je n'ai pas d'actualités récents à montrer pour le moment...</p>
+        <?php endif; ?>
+
+    </div>
+
+    <div class="bouton_container">
+        <a class="bouton_foyer" href="<?= $bouton_foyer['url'] ?>"><?= $bouton_foyer['title'] ?></a>
+    </div>
+
 </section>
 
-<section>
-    <h2>$titre_section_foyer</h2>
-    <a href="<?= $bouton_foyer['url'] ?>"><?= $bouton_foyer['title'] ?></a>
+
+<section class="faq">
+    <h2 class="titre_faq"><?= $titre_faq ?></h2>
+    <?php
+    if (have_rows('faq')):
+        while (have_rows('faq')): the_row();
+            $titre_question = get_sub_field('titre_question');
+            ?>
+            <p class="titre_question"><?= $titre_question ?></p>
+        <?php
+        endwhile;
+    endif;
+    ?>
 </section>
+
+
+<section class="galerie">
+    <img class="one_line_image_galerie" src="<?= $one_line_image_galerie['url'] ?>"
+         alt="<?= $one_line_image_galerie['alt'] ?>">
+
+    <h2 class="titre_galerie"><?= $titre_galerie ?></h2>
+
+    <div class="image_galerie_container">
+        <?php foreach ($galerie_photo as $image): ?>
+            <img class="galerie_photo" src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>">
+        <?php endforeach; ?>
+    </div>
+</section>
+
 
 <?php get_footer(); ?>
